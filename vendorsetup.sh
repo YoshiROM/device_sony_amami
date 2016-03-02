a291d5dbeeb382c32de3db910a409918fd17df1f
SpiritCroc <spiritcroc@gmail.com>
2016-03-02 16:46:06 +0100

    Yoshi-fy
    Change-Id: I3d4d227b45fbbbcdd4ac4760bdf6d07fc051e3d5

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo slim_amami-userdebug
+add_lunch_combo yoshi_amami-userdebug
--- yoshi.mk
+++ yoshi.mk
@@ -3,13 +3,13 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/slim/config/gsm.mk)
+$(call inherit-product, vendor/yoshi/config/gsm.mk)
 
-# Inherit Slim common Phone stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+# Inherit Yoshi common Phone stuff.
+$(call inherit-product, vendor/yoshi/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/yoshi/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/sony/amami/full_amami.mk)
@@ -19,7 +19,7 @@ PRODUCT_RELEASE_NAME := amami
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := amami
-PRODUCT_NAME := slim_amami
+PRODUCT_NAME := yoshi_amami
 PRODUCT_BRAND := Xperia
 PRODUCT_MODEL := Z1 Compact
 PRODUCT_MANUFACTURER := Sony
